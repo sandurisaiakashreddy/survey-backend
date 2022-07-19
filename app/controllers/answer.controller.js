cffd744f24eb2e138ec6564757db794036182ba3
rishikeshsamanapelly <r.samanapelly@eagles.oc.edu>
2022-07-19 17:24:25 -0500

    Delete survey API

---
+++ app/controllers/answer.controller.js
@@ -0,0 +1,175 @@
+const db = require("../models");
+const Question = db.question;
+const Answer = db.answer;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Question
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.description) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  //Submit a Survey into Answer
+  const answerrequest = {
+    surveyid: req.body.surveyid, 
+    userid: req.body.userid,
+    description: req.body.description,
+    type: req.body.type,
+    options: req.body.options,
+    answer: req.body.answer
+  };
+
+  // Save Answer in the database
+  Answer.create(answerrequest)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while submitting the answer."
+      });
+    });
+};
+
+  
+// Retrieve all questions from the database.
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+
+  Question.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Questions."
+      });
+    });
+};
+
+// Retrieve all songs from the database based on album id.
+exports.findAllBySurveyId = (req, res) => {
+    const surveyId = req.params.id;
+
+    Question.findAll({  where: { surveyid: surveyId } })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving Question by survey id."
+        });
+      });
+  };
+
+// Find a single Question with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Question.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Question with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Question with id=" + id
+      });
+    });
+};
+
+// Update a Question by the id in the request
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Question.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Question was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Question with id=${id}. Maybe Question was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Question with id=" + id
+      });
+    });
+};
+
+// Delete a Question with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Question.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Question was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Question with id=${id}. Maybe Question was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Question with id=" + id
+      });
+    });
+};
+
+// Delete all Questions from the database.
+exports.deleteAll = (req, res) => {
+  Question.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Questions were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Questions."
+      });
+    });
+};
+
+exports.findAllByArtist = (req, res) => {
+  const artist = req.query.artist;
+  var condition = artist ? { artist: { [Op.like]: `%${artist}%` } } : null;
+
+  Question.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Questions By Artist Name."
+      });
+    });
+};
